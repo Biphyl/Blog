0e3f51c155c787ded6adeaf8cef9920497c68341
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-16 12:12:55 +0300

    modified models and manage.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask_login import UserMixin
 from blog import db
 
 class User(UserMixin,db.Model):
