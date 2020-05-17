dff309d8ff758d95c304d13d13c00ce9c5cb7b64
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-17 10:46:51 +0300

    fix errors

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = 'Lovine'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2:/biron:Biron4745@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
