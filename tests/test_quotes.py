52464502f3f83cdac21e98d7cb3b2b615bea3de2
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-18 09:43:04 +0300

    modified quotes test

--- tests/test_quotes.py
+++ tests/test_quotes.py
@@ -3,7 +3,7 @@ from blog.models import Quotes
 
 class testQuotes(unittest.TestCase):
     def setUp(self):
-        self.new_quote = Quotes('morimga','Coding can be fun',"http://github.com/Biphyl")
+        self.new_quote = Quotes('morimga','Coding can be fun')
 
     def test_instance_variables(self):
         self.assertTrue(isinstance(self.new_quote,Quotes))
